483c07d935ba6221c9987a02e73f35471b5947fa
DANKnespl <100779974+DANKnespl@users.noreply.github.com>
2022-10-12 19:37:40 +0200

    Create morse.h

---
+++ Morseovka/morse.h
@@ -0,0 +1,3 @@
+//void Symbol(double symbol,double pLetter,DigitalOut myled,int ledOn,int ledOff);
+//void Letter(char letter,double dot,double dash,double pLetter,DigitalOut myled,int ledOn,int ledOff);
+void Word(char in[10],double dot,double dash,double pLetter,double pWord, DigitalOut myled,int ledOn,int ledOff);
